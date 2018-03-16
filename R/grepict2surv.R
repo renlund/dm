7f4303ea379b54cc892e5ae36808bb8e646deb14
Henrik Renlund <henrikrenlund@gmail.com>
2018-03-16 16:00:54 +0100

    grepict2surv

---
+++ R/grepict2surv.R
@@ -0,0 +1,28 @@
+##' @title turn grepict alias into survival data
+##' @description make a grepict long format into wide survival data variables
+##' @param g returned from grepict (long format, stacked)
+##' @param prefix prefix for time and event variable
+##' @return a data frame
+##' @export
+grepict2surv <- function(g, prefix = c('time' = 't.', 'event' = 'ev.')){
+    .required_properties(x = prefix, class = 'character',
+                         length = 2, nm = 'prefix')
+    .required_data_names(data.names = names(g),
+                         required = c('id', 'time', 'event', 'first.id'))
+    G <- subset(g, first.id == 1)
+    AL <- unique(G$alias)
+    R <- NULL
+    if(length(AL) != 0){
+        for(i in seq_along(AL)){
+            nm <- AL[i]
+            d <- subset(G, G$alias == nm, select = c('id', 'time', 'event'))
+            names(d) <- c('id', paste0(prefix, nm))
+            R <- if(is.null(R)) d else merge(R, d, by = 'id')
+        }
+        rownames(R) <- NULL
+        R
+    } else {
+        message("that's odd, no values in 'alias")
+        as.data.frame(R)
+    }
+}
