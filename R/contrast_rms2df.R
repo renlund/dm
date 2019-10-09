b1df15946387757576064e9e3955753b2989104e
Henrik Renlund <henrikrenlund@gmail.com>
2019-10-09 10:32:38 +0200

    added contrast_rms2df and contrast_rms2tidy

---
+++ R/contrast_rms2df.R
@@ -0,0 +1,27 @@
+##' contrast to data frame
+##'
+##' turn a contrast.rms object into a data frame
+##' @param c a contrast rms object
+##' @return a data.frame
+##' @export
+contrast_rms2df <- function(c){
+    rm <- c("var", "df.residual", "X", "cnames", "nvary",
+            "conf.type", "conf.int", "redundant")
+    for(k in rm) c[k] <- NULL
+    class(c) <- "list"
+    as.data.frame(c)
+}
+
+##' @describeIn  contrast_rms2df mimick \code{broom::tidy} behaviour  as applied
+##'     to a contrast.rms object
+##' @export
+contrast_rms2tidy <- function(c){
+    x <- contrast_rms2df(c)
+    names(x)[names(x) == "Contrast"] <- "estimate"
+    names(x)[names(x) == "SE"] <- "std.error"
+    names(x)[names(x) == "Z"] <- "statistic"
+    names(x)[names(x) == "Lower"] <- "conf.low"
+    names(x)[names(x) == "Upper"] <- "conf.high"
+    names(x)[names(x) == "Pvalue"] <- "p.value"
+    x
+}
