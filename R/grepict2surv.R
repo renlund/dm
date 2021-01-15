90c54509057488d80a812445555c52e1d1b6e863
Henrik Renlund <henrikrenlund@gmail.com>
2021-01-15 15:04:16 +0100

    coding of grepict2surv only worked for unique id

--- R/grepict2surv.R
+++ R/grepict2surv.R
@@ -8,16 +8,16 @@ grepict2surv <- function(g, prefix = c('time' = 't.', 'event' = 'ev.')){
     .required_properties(x = prefix, class = 'character',
                          length = 2, nm = 'prefix')
     .required_data_names(data.names = names(g),
-                         required = c('id', 'time', 'event', 'first.id'))
+                         required = c('id', 'begin', 'time', 'event', 'first.id'))
     G <- subset(g, g$first.id == 1)
     AL <- unique(G$alias)
     R <- NULL
     if(length(AL) != 0){
         for(i in seq_along(AL)){
             nm <- AL[i]
-            d <- subset(G, G$alias == nm, select = c('id', 'time', 'event'))
-            names(d) <- c('id', paste0(prefix, nm))
-            R <- if(is.null(R)) d else merge(R, d, by = 'id')
+            d <- subset(G, G$alias == nm, select = c('id', 'begin', 'time', 'event'))
+            names(d) <- c('id', 'begin', paste0(prefix, nm))
+            R <- if(is.null(R)) d else merge(R, d, by = c('id', 'begin'))
         }
         rownames(R) <- NULL
         R
