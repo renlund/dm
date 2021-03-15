cbf4f8090a92b951aaa7a4b49b3bac63504482ba
Henrik Renlund <henrikrenlund@gmail.com>
2021-03-15 13:13:32 +0100

    cluster.by added (work in progress)

---
+++ R/kin.R
@@ -0,0 +1,29 @@
+##' clustering
+##'
+##' create cluster variable indicated by other variable
+##' @param x variable from which to create cluster
+##' @param by
+##' @export
+cluster.by <- function(x, by){
+    if(length(x) == 0 | is.null(x)){
+        warning("'x' is too little (NULL or length 0)")
+        return(invisible(NULL))
+    }
+    .required_properties(by, class = "character", length = 1)
+    if(by == "incl.next"){
+        cluster.by.incl.next(incl.next = x)
+    } else {
+        s <- paste0("by = '", by, "' currently not supported")
+        warning(s)
+        invisible(NULL)
+    }
+}
+
+##' @describeIn cluster.by cluster by 'include.next' indicator
+##' @param incl.next a logical variable that for each line include wether the
+##'     next line should belong to the same cluster as the current line
+##' @export
+cluster.by.incl.next <- function(incl.next){
+    n <- length(incl.next)
+    c(1, 1+cumsum(!incl.next[-n]))
+}
---
+++ tests/testthat/test_cluster_by.r
@@ -0,0 +1,58 @@
+test_that("'cluster.by' works", code = {
+
+    df <- data.frame(
+        grouping.wanted = factor(LETTERS[c(1, 1, 2, 2, 3, 4, 5)]),
+        next.incl = c(T, F, T, F, F, F, F)
+        )
+    df$create <- cluster.by.incl.next(incl.next = df$next.incl)
+    ## df ## visually inspect
+
+    expect_equal(
+        cluster.by.incl.next(df$next.incl),
+        as.numeric(df$grouping.wanted)
+    )
+
+    v <- c(T,T,F,T,F)
+    expect_equal(
+        cluster.by.incl.next(v),
+        c(1,1,1,2,2)
+    )
+    v2 <- v; v2[5] <- !v[5]
+    expect_equal(
+        cluster.by.incl.next(v),
+        cluster.by.incl.next(v2)
+    )
+    v2[5] <- NA
+    expect_equal(
+        cluster.by.incl.next(v),
+        cluster.by.incl.next(v2)
+    )
+
+    ## random test!
+    seed <- sample(1:10000000000, 1)
+    set.seed(seed)
+    n <- 10000
+    m <- 15
+    start <- sample(seq(1, n, by = 10), size = m, replace = FALSE)
+    lag_start <- c(start[2:m], n+1)
+    stop <- ifelse(rbinom(m, 1, 0.6),
+                   yes = lag_start,
+                   no = lag_start - 5)
+    next.incl <- stop == lag_start
+    r <- rep(NA, m)
+    r[1] <- 1
+    for(i in 2:m){
+        if(start[i] == stop[i-1]){
+            r[i] <- r[i-1]
+        } else {
+            r[i] <- r[i-1] + 1
+        }
+    }
+    expect_equal(
+        cluster.by.incl.next(incl.next = next.incl),
+        r
+    )
+    data.frame(start, stop, r,
+               calc = cluster.by.incl.next(incl.next = next.incl))
+
+})
